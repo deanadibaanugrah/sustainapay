91168956d613adc9b20a69a69c067d2ff163790d
deanadibaanugrah <deanadibaanugrah@gmail.comm>
2026-06-05 23:00:41 +0700

    Remove echo test from Vercel index

--- sustainapay-backend/api/index.php
+++ sustainapay-backend/api/index.php
@@ -5,13 +5,13 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-echo "TEST_VERCEL_PHP_EXECUTION\n";
-
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['CACHE_STORE'] = 'array';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
