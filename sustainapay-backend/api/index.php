e06626c22f82df58658ecf4e1cb63cbb57decd23
deanadibaanugrah <deanadibaanugrah@gmail.comm>
2026-06-05 22:52:52 +0700

    Enable error reporting to debug empty 500 internal server error

--- sustainapay-backend/api/index.php
+++ sustainapay-backend/api/index.php
@@ -1,6 +1,10 @@
 <?php
 
 // Fix Vercel serverless read-only filesystem issues
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
