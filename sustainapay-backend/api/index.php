51b89d17dd1afc8a13cab958f8731274f73dabdf
deanadibaanugrah <deanadibaanugrah@gmail.comm>
2026-06-05 23:08:42 +0700

    Remove forced APP_DEBUG true for production safety

--- sustainapay-backend/api/index.php
+++ sustainapay-backend/api/index.php
@@ -9,7 +9,6 @@ $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['CACHE_STORE'] = 'array';
-$_ENV['APP_DEBUG'] = 'true';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['PHP_SELF'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
