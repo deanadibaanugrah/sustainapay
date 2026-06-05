a7dd63f652453ca85d6381a445e4cf820840d92f
deanadibaanugrah <deanadibaanugrah@gmail.comm>
2026-06-05 22:59:39 +0700

    Fix SCRIPT_NAME to prevent Laravel from stripping /api prefix and throwing 404

--- sustainapay-backend/api/index.php
+++ sustainapay-backend/api/index.php
@@ -11,6 +11,7 @@ $_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 $_ENV['CACHE_STORE'] = 'array';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
 
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
