f579ed55e488ee79d5b96e55273c0394a9c99ca2
deanadibaanugrah <deanadibaanugrah@gmail.comm>
2026-06-05 23:06:12 +0700

    Fix environment variables untrimmed values using putenv for Vercel

--- sustainapay-backend/api/index.php
+++ sustainapay-backend/api/index.php
@@ -14,5 +14,11 @@ $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['PHP_SELF'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
+if (isset($_ENV['DB_HOST'])) { $_ENV['DB_HOST'] = trim($_ENV['DB_HOST'], " \t\n\r\0\x0B\"'"); putenv('DB_HOST=' . $_ENV['DB_HOST']); }
+if (isset($_ENV['DB_PORT'])) { $_ENV['DB_PORT'] = trim($_ENV['DB_PORT'], " \t\n\r\0\x0B\"'"); putenv('DB_PORT=' . $_ENV['DB_PORT']); }
+if (isset($_ENV['DB_DATABASE'])) { $_ENV['DB_DATABASE'] = trim($_ENV['DB_DATABASE'], " \t\n\r\0\x0B\"'"); putenv('DB_DATABASE=' . $_ENV['DB_DATABASE']); }
+if (isset($_ENV['DB_USERNAME'])) { $_ENV['DB_USERNAME'] = trim($_ENV['DB_USERNAME'], " \t\n\r\0\x0B\"'"); putenv('DB_USERNAME=' . $_ENV['DB_USERNAME']); }
+if (isset($_ENV['DB_PASSWORD'])) { $_ENV['DB_PASSWORD'] = trim($_ENV['DB_PASSWORD'], " \t\n\r\0\x0B\"'"); putenv('DB_PASSWORD=' . $_ENV['DB_PASSWORD']); }
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
